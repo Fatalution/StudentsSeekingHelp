e65e8da020795274ee3fb8aabe2434d703468cf8
Simona Paulescu <simona.alexandra.paulescu@gmail.com>
2017-02-23 16:54:59 +0000

    added database

--- groupproject.sql
+++ groupproject.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2017 at 11:04 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Feb 23, 2017 at 04:52 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `ID` int(200) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Description` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`ID`, `Name`, `Description`) VALUES
+(1, 'COMP15112', 'Fundamentals of Computer Architecture is the best course to not understand shit'),
+(2, 'COMP1211', 'Fundamentals of Computer Engineering is the best course to not understand shit'),
+(3, 'COMP1112', 'Maths, what the fuck is this'),
+(4, 'COMP10120', 'First year project, where no one goes to the lectures'),
+(5, 'COMP16112', 'java, where Latham makes dad jokes and only he laughs at them');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -30,32 +53,26 @@ CREATE TABLE `user` (
   `ID` int(11) NOT NULL,
   `Email` text NOT NULL,
   `Password` text NOT NULL,
-  `Year` int(11) NOT NULL
+  `Admin` tinyint(1) NOT NULL DEFAULT '0',
+  `Course_type` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ID`, `Email`, `Password`, `Year`) VALUES
-(1, 'ivandrej@gmail.com', '1234', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL,
-  `Name` text NOT NULL,
-  `Email` int(11) NOT NULL,
-  `Year` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `user` (`ID`, `Email`, `Password`, `Admin`, `Course_type`) VALUES
+(1, 'ivandrej@gmail.com', '1234', 0, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `user`
@@ -64,25 +81,19 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD UNIQUE KEY `ID` (`ID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `ID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
