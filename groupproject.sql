9384decd812c9126369de0b5f4600c1747a7a5ec
Simona Paulescu <simona.alexandra.paulescu@gmail.com>
2017-03-02 16:20:53 +0000

    Used the cover pic for the first page and modified the databaase

--- groupproject.sql
+++ groupproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2017 at 09:07 AM
+-- Generation Time: Mar 02, 2017 at 04:17 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -29,19 +29,23 @@ SET time_zone = "+00:00";
 CREATE TABLE `courses` (
   `ID` int(200) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Description` text NOT NULL
+  `Description` text NOT NULL,
+  `computer_science` tinyint(1) NOT NULL DEFAULT '0',
+  `computer_science_maths` tinyint(1) NOT NULL DEFAULT '0',
+  `computer_science_business` tinyint(1) NOT NULL DEFAULT '0',
+  `human_computer_interaction` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `courses`
 --
 
-INSERT INTO `courses` (`ID`, `Name`, `Description`) VALUES
-(1, 'COMP15112', 'Fundamentals of Computer Architecture is the best course to not understand shit'),
-(2, 'COMP1211', 'Fundamentals of Computer Engineering is the best course to not understand shit'),
-(3, 'COMP1112', 'Maths, what the fuck is this'),
-(4, 'COMP10120', 'First year project, where no one goes to the lectures'),
-(5, 'COMP16112', 'java, where Latham makes dad jokes and only he laughs at them');
+INSERT INTO `courses` (`ID`, `Name`, `Description`, `computer_science`, `computer_science_maths`, `computer_science_business`, `human_computer_interaction`) VALUES
+(1, 'COMP15112', 'Fundamentals of Computer Architecture is the best course to not understand shit', 0, 0, 0, 0),
+(2, 'COMP1211', 'Fundamentals of Computer Engineering is the best course to not understand shit', 0, 0, 0, 0),
+(3, 'COMP1112', 'Maths, what the fuck is this', 0, 0, 0, 0),
+(4, 'COMP10120', 'First year project, where no one goes to the lectures', 0, 0, 0, 0),
+(5, 'COMP16112', 'java, where Latham makes dad jokes and only he laughs at them', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -96,15 +100,17 @@ CREATE TABLE `user` (
   `Admin` tinyint(1) NOT NULL DEFAULT '0',
   `Course_type` varchar(200) NOT NULL,
   `Name` text NOT NULL,
-  `Surname` text NOT NULL
+  `Surname` text NOT NULL,
+  `points` int(11) NOT NULL,
+  `ranking` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ID`, `Email`, `Password`, `Admin`, `Course_type`, `Name`, `Surname`) VALUES
-(1, 'andrej.ivanov@manchester.ac.uk', '$2y$10$NQNhTwRg/C98lrpo9inrj.aHFU5MVEk7Bv6Q7TGmikPKU8ASM7V0O', 0, 'Computer Science', 'Andrej', 'Ivanov');
+INSERT INTO `user` (`ID`, `Email`, `Password`, `Admin`, `Course_type`, `Name`, `Surname`, `points`, `ranking`) VALUES
+(1, 'andrej.ivanov@manchester.ac.uk', '$2y$10$NQNhTwRg/C98lrpo9inrj.aHFU5MVEk7Bv6Q7TGmikPKU8ASM7V0O', 0, 'Computer Science', 'Andrej', 'Ivanov', 0, '');
 
 --
 -- Indexes for dumped tables
