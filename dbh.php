b004d4f064536a424df364770b33eb169cd9bc8c
Andrej Ivanov <ivandrej@gmail.com>
2017-03-18 01:56:35 +0000

    Added dbh again, don't put it in git ignore

---
+++ dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+//Connect to the local database 
+$conn = mysqli_connect('localhost', 'root', '', 'groupproject');
+
+// If connection failed, display the connection error
+if(!$conn)
+{
+	die("Failed to connect: " . mysqli_connect_error());
+}
